2f5ec4bb013ccc872827f1f10a6366aa175b2d3c
Djed01 <gordonletic5@gmail.com>
2023-10-23 19:54:51 +0200

    Login and main window

---
+++ RentACar/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using RentACar.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.ViewModel
+{
+
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand CarsViewCommand { get; set; }
+        public RelayCommand CustomersViewCommand { get; set; }
+
+        public CarsViewModel CarsVM { get; set; }
+
+        public CustomersViewModel CustomersVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            CarsVM = new CarsViewModel();
+            CustomersVM = new CustomersViewModel();
+            CurrentView = CarsVM;
+
+            CarsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CarsVM;
+            });
+
+            CustomersViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CustomersVM;
+            });
+        }
+    }
+}
